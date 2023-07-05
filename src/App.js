c120d3759d53994a9e3bb65c9be20c62563bcf76
Konvoir <baleskoff@gmail.com>
2023-07-05 10:37:47 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
-import axios from 'axios';
-import Counter from "./components/Counter";
+import React, {useState} from "react";
+// import axios from 'axios';
+// import Counter from "./components/Counter";
 
 function App() {
+  const [value, setValue] = useState('Текс и инпуте')
+
 
   return (
-    <div className="wrapper">
-      <div className="app">
-        <div className="app__body">
-          
-<Counter/>
+        <div className="app">
+         <div className="post">
+          <div className="post__content">
+            <strong>1. Javascript</strong>
+            <div>
+            Javascript - мой любимый язык
+            </div>
+          </div>
+         </div>
+
         </div>
-      </div>
-    </div>
-  );
+        );
 }
 
 export default App;
