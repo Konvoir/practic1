e2d650ba66d615fa808d482680fa36a65d967562
Konvoir <baleskoff@gmail.com>
2023-07-05 13:25:03 +0300

    posts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React, {useState} from "react";
 import './styles/App.css'
+import PostItem from "./components/PostItem/PostItem";
 // import axios from 'axios';
 // import Counter from "./components/Counter";
 
 function App() {
-  // const [value, setValue] = useState('Текс и инпуте')
+  const [posts, setPosts] = useState([
+      {id: 1, title: 'Javascript', body:'мой любимый язык'},
+      {id: 2, title: 'React', body:'мой любимая библиотека'},
+      {id: 3, title: 'React native', body:'приоритет в изучении'},
+      {id: 4, title: 'Node', body:'важный язык'}
+  ])
 
 
   return (
         <div className="App">
-         <div className="post">
-          <div className="post__content">
-            <strong>1. Javascript</strong>
-            
-            <div>
-            Javascript - мой любимый язык
-            </div>
-          </div>
-          <div className="post__btns">
-            <button>Удалить</button>
-          </div>
-         </div>
-
+          {posts.map(post =>
+              <PostItem post={post} key={post.id}/>
+            )}
         </div>
         );
 }
