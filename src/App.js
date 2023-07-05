7a8f6c92ae6a968cbc3a4306a5c62f48b02f08f5
Konvoir <baleskoff@gmail.com>
2023-07-05 12:48:18 +0300

    post

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, {useState} from "react";
+import './styles/App.css'
 // import axios from 'axios';
 // import Counter from "./components/Counter";
 
 function App() {
-  const [value, setValue] = useState('Текс и инпуте')
+  // const [value, setValue] = useState('Текс и инпуте')
 
 
   return (
-        <div className="app">
+        <div className="App">
          <div className="post">
           <div className="post__content">
             <strong>1. Javascript</strong>
+            
             <div>
             Javascript - мой любимый язык
             </div>
+          </div>
+          <div className="post__btns">
+            <button>Удалить</button>
           </div>
          </div>
 
